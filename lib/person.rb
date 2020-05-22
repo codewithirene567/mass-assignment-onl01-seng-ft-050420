36f18bf077613389e8b9eb2ac7d7f23e9e91ee0e
Irene Scott <irenejinscott@gmail.com>
2020-05-22 23:33:18 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,6 +6,8 @@ attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 
 
   def initialize(attributes)
-attributes.each {|key, value| self.send (("#{key}="), value)}
+attributes.each do |key, value|
+  self.send (("#{key}="), value)}
+   end
+   end
 end
-    end
