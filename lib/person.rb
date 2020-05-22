1692bb04257ce28b9e8e335afa90dd49457eae3c
Irene Scott <irenejinscott@gmail.com>
2020-05-22 23:28:19 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,15 @@
 class Person
 
-attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
-:weight, :handed, :complexion, :t_shirt_size,
+attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
+
 
   def initialize(attributes)
 attributes.each {|key, value| self.send (("#{key}=")value)}
     
     end
 end
+
+joe_attributes = {name: "Joe", :birthday "June"}
+joe = Person.new(joe_attributes)
